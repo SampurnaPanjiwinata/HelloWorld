bb7357841435902e6330401919061bb1541b9066
SampurnaPanjiwinata <96180757+SampurnaPanjiwinata@users.noreply.github.com>
2022-04-17 11:23:47 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const HOST = '0.0.0.0'
 const app = express()
 app.get('/', (req, res) => {
     res.send('Hello world!\n')
+    res.send('Sampurna Panjiwinata \n')
+    res.send('52019034 \n')
+    res.send('Informatika')
 })
 
 app.listen(PORT, HOST)
